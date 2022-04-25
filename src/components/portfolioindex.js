b0e811609731e277c72b544cd02f341be838cc09
Kody McIntosh <kodymcintosh@developersinstitute.co.nz>
2022-04-25 16:32:10 +1200

    adding header text and style to portfolio page

--- src/components/portfolioindex.js
+++ src/components/portfolioindex.js
@@ -4,31 +4,41 @@ import portfolioData from "./portfolioData";
 
 const PortfolioIndex = () => {
   return (
-    <div className="portfolioGrid">
-      <ul>
-        {Object.entries(portfolioData).map(
-          ([slug, { name, subtitle, featureImage, tags }]) => (
-            <li
-              key={slug}
-              className="gridBackImage"
-              style={{
-                backgroundImage: `url(${featureImage})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-              }}
-            >
-              <Link to={`/portfolio/${slug}`}>
-                <div className="portfolioIndexTitle">
-                  <h3>{name}</h3>
-                  <h4>{subtitle}</h4>
-                  <p>{tags}</p>
-                </div>
-              </Link>
-            </li>
-          )
-        )}
-      </ul>
-    </div>
+    <>
+      <h1 className="portfolioTitle">Portfolio</h1>
+      <p className="portfolioIntro">
+        <strong>These are just some of my favourite portfolio works.</strong>
+      </p>
+      <p className="portfolioIntro">
+        For a full portfolio, or to ask about specific kinds of work I have
+        completed, please <a href="/contact/">contact me.</a>
+      </p>
+      <div className="portfolioGrid">
+        <ul>
+          {Object.entries(portfolioData).map(
+            ([slug, { name, subtitle, featureImage, tags }]) => (
+              <li
+                key={slug}
+                className="gridBackImage"
+                style={{
+                  backgroundImage: `url(${featureImage})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Link to={`/portfolio/${slug}`}>
+                  <div className="portfolioIndexTitle">
+                    <h3>{name}</h3>
+                    <h4>{subtitle}</h4>
+                    <p>{tags}</p>
+                  </div>
+                </Link>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
