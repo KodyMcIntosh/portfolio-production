c149b67703d663ce1b4b3c336526220db32cce46
Kody McIntosh <kodymcintosh@developersinstitute.co.nz>
2022-04-25 22:39:13 +1200

    adding more portfolio content

--- src/components/portfolioData.js
+++ src/components/portfolioData.js
@@ -1,11 +1,199 @@
 const portfolioData = {
+  "liberate-it-website": {
+    name: "Liberate I.T.",
+    subtitle: "Website",
+    projectBrief:
+      "Design and build new websites for New Zealand and Australia based on the newly created branding, and brand positioning.",
+    description: `<p><strong>This project was part of an overall overhaul that I completed in 2019 for Liberate I.T. and included many pieces of collateral as well as the website. </strong></p>
+  
+    <p>This included not just a design and build of the framework, but also completely reworking the content and structure. It had to be fully mobile responsive, and had to communicate to a very specific audience. This involved a balance of technical terminology, and general explanation.</p>
+
+    <p>Because Liberate I.T. operate in both New Zealand and Australia, and they have specific offerings for each place, they wanted to have two separate sites using two location-specific domains. This meant creating a balance between content across both, and setting up a technical environment that would still be easy to manage for both interfaces.</p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress. Optimised for page speed, accessibility, and mobile responsiveness.</p><p>This website was built using multisite, and hosted on an excellent WordPress-specific hosting platform. It was created and built with performance and easy maintenance in mind, with a particular focus toward mobile usability. It utilised CDN content serving, image compression optimisation, and dynamic loading to aid in its speed.</p><p>Liberate I.T. have since made the switch to a single site for both locations, and made some changes to their overall design.</p>",
+    featureImage: `/liberate/home-tablet-1.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/liberate/home-tablet-1.jpeg`,
+        imageAlt: `Liberate I.T. website mock up.`,
+      },
+      {
+        imageSrc: `/liberate/homeScroll.jpeg`,
+        imageAlt: `Liberate I.T. website mock up.`,
+      },
+      {
+        imageSrc: `/liberate/tabletTeamNew.jpeg`,
+        imageAlt: `Liberate I.T. website mock up.`,
+      },
+    ],
+  },
+  "liberate-it-edm": {
+    name: "Liberate I.T.",
+    subtitle: "Email Marketing",
+    projectBrief:
+      "Create monthly emails for communicating with the existing customer base.",
+    description: `<p>As part of my work with Liberate I.T. I designed and built monthly Info Hub messaging for their existing customer base. This was set up as a means of communicating what events were coming up, free webinars customers could join in on, promotional offers, and helping them get to know the wider staff.</p>
+  
+    <p>These EDMs were created by hard-coding custom HTML from scratch, using a series of templates that I created for them. I then loaded their emails into NetSuite (their internal ERP system including CRM functionality), and sent it out to the customer base from there.</p>
+`,
+    technicalSpec: "<p>Built using HTML and CSS, distributed via NetSuite.</p>",
+    featureImage: `/liberate/edm1.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/liberate/edm1.jpeg`,
+        imageAlt: `Liberate I.T. Info Hub EDM.`,
+      },
+      {
+        imageSrc: `/liberate/edmLong-1.jpeg`,
+        imageAlt: `Liberate I.T. Info Hub EDM.`,
+      },
+    ],
+  },
+  inugo: {
+    name: "Inugo",
+    subtitle: "Website",
+    projectBrief:
+      "Rollout and build a WordPress website based on a supplied partial design.",
+    description: `<p><strong>Inugo is a parking app that is trying to change the way we park the world over.</strong></p>
+  
+    <p>While I was managing the website through 2017 and 2018, the app was active both within New Zealand and also America. Because of this, the client needed the site to be set up to serve messaging specific to different audiences and locations, without compromising performance.</p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress. Utilises WordPress, internal CRM, user geolocation-specific content. Optimised for page speed, accessibility, and mobile responsiveness.</p><p>This website was built using WordPress, and integrated with the client's internal CRM, which required custom coding to achieve.</p><p>This meant that optimisation was a priority, as the custom aspects of the build had the potential to slow down site performance. I was able to achieve this and create a happy medium.</p><p>The content within the website was configured to serve whatever message was the most appropriate for the user based on their geolocation. Both within-page content, and also full page url-based redirection was included to accommodate this.</p> ",
+    featureImage: `/inugo/Inugo-featuredWEB.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/inugo/Inugo-featuredWEB.jpeg`,
+        imageAlt: `Inugo website mock up.`,
+      },
+      {
+        imageSrc: `/inugo/Inugo-aboutWEB.jpeg`,
+        imageAlt: `Inugo website mock up.`,
+      },
+      {
+        imageSrc: `/inugo/Inugo-fcbWEB.jpeg`,
+        imageAlt: `Inugo website mock up.`,
+      },
+      {
+        imageSrc: `/inugo/Inugo-home2WEB.jpeg`,
+        imageAlt: `Inugo website mock up.`,
+      },
+    ],
+  },
+  "this-is-electric": {
+    name: "This Is Electric",
+    subtitle: "Website",
+    projectBrief: "Design and build a website based on a newly created brand.",
+    description: `<p>This Is Electric is an Auckland-based electrician business. Their existing website wasn't performing well for them, and they needed a full overhaul to meet their needs within their industry. I worked with them to design, build, and create all of the content on their new site. This meant getting to know their points of difference, and their services well enough to create useful and meaningful copy to accompany the design.</p>
+
+    <p className="button"><a href="https://thisiselectric.co.nz/" target="_blank" rel="noreferrer">View Live Site</a></p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress. Optimised for page speed, accessibility, mobile responsiveness, easy updates.</p>",
+    featureImage: `/this-is-electric/home.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/this-is-electric/home.jpeg`,
+        imageAlt: `This Is Electric website mock up.`,
+      },
+      {
+        imageSrc: `/this-is-electric/home1.jpeg`,
+        imageAlt: `This Is Electric website mock up.`,
+      },
+      {
+        imageSrc: `/this-is-electric/blog.jpeg`,
+        imageAlt: `This Is Electric website mock up.`,
+      },
+      {
+        imageSrc: `/this-is-electric/services.jpeg`,
+        imageAlt: `This Is Electric website mock up.`,
+      },
+    ],
+  },
+
+  "miss-marie-web": {
+    name: "Miss Marie",
+    subtitle: "Website",
+    projectBrief:
+      "Design and build an e-commerce website, including all brand elements, photography, and payment system integration.",
+    description: `<p>This project involved full roll out across a range of collateral, both online and in print. Miss Marie was a passion project of mine. It was an online store that sold pinup, gothic, steampunk and alternative clothing. </p>
+  
+    <p>The website needed to be easy to use, fit the theme of the clothing styles, and be fast to load. There also needed to be the capacity for custom product filtering, as well as detailed product searches. I set up a full content management system, making content updates easy, as well as adding or removing of products. It also made it simple to share content across multiple platforms. It was fully mobile responsive.</p>
+
+    <p>The photography was completed in conjunction with Jessie Rolton Photography. This included some of the products shots, as well as a model shoot showcasing the most popular stock.</p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress and WooCommerce. Optimised for page speed and accessibility.</p>",
+    featureImage: `/miss-marie/website-iPad-homepageWEB.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/miss-marie/website-blog-ipadWEB.jpeg`,
+        imageAlt: `Miss Marie website mock up.`,
+      },
+      {
+        imageSrc: `/miss-marie/website-iPad-homepageWEB.jpeg`,
+        imageAlt: `Miss Marie website mock up.`,
+      },
+      {
+        imageSrc: `/miss-marie/website-product-pageWEB.jpeg`,
+        imageAlt: `Miss Marie website mock up.`,
+      },
+    ],
+  },
+  "dns-forest-products": {
+    name: "DNS",
+    subtitle: "Website",
+    projectBrief:
+      "Build a website and rollout the design based on a homepage design and provided assets.",
+    description: `<p><strong>Working in conjunction with <a href="http://www.chriswardcreative.com/" target="_blank" rel="noreferrer">Chris Ward Creative</a>, I created a new website for DNS Forest Products. The key aims included ease of use, ease of updates, clarity of information, and being fully mobile responsive. It also needed to have a working translation option for three languages.</strong></p>
+
+    <p>After receiving the design elements I discussed options around building, so that all of these objectives could be met. I provided the client with a full CMS that they could update themselves, as well as documentation on how to do so.</p>
+
+    <p>I was able to create a clean, well-functioning website build that was accurate to the creative provided.</p>
+
+    <p className="button"><a href="http://dnsltd.co.nz/" target="_blank" rel="noreferrer">View Live Site</a></p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress. Optimised for page speed, accessibility, mobile responsiveness, and easy updates.</p>",
+    featureImage: `/dns-forest-products/DNS-ipadWEB.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/dns-forest-products/DNS-ipadWEB.jpeg`,
+        imageAlt: `DNS website mock up on ipad.`,
+      },
+      {
+        imageSrc: `/dns-forest-products/DNS-laptopWEB.jpeg`,
+        imageAlt: `DNS website mock up on laptop.`,
+      },
+    ],
+  },
+  "mood-hair": {
+    name: "Mood Hair",
+    subtitle: "Website",
+    projectBrief: "Design and build a single page website to promote services.",
+    description: `<p><strong>Mood Hair Studio is based in Drury, Auckland, and went through a rebrand in 2019. After they had done this, they approached me about creating a website to match.</strong></p>
+    <p>They only wanted a simple solution, one that listed their services while also encouraging click through to their existing booking platform.</p>
+    
+    <p className="button"><a href="http://www.moodhair.nz/" target="_blank" rel="noreferrer">View Live Site</a></p>
+`,
+    technicalSpec:
+      "<p>Built using WordPress. Optimised for page speed and accessibility.</p>",
+    featureImage: `/mood-hair/mood.jpeg`,
+    imageGallery: [
+      {
+        imageSrc: `/mood-hair/mood.jpeg`,
+        imageAlt: `Mood hair studio one page website mock up.`,
+      },
+    ],
+  },
   "liberate-it-branding": {
     name: "Liberate I.T.",
     subtitle: "Branding",
-    projectBrief: `Create an entirely new logo, variants, and full brand positioning.`,
-    description: `<p>This project was part of a wider overhaul that I completed over 2018 – 2019 for Liberate I.T. and included many pieces of collateral.</p>
+    projectBrief: `Create a new logo, variants, and full brand positioning.`,
+    description: `<p>This project was part of a wider overhaul that I completed over 2018 - 2019 for Liberate I.T. and included many pieces of collateral.</p>
     
-    <p>Liberate I.T. are a top level NetSuite Solution Provider in New Zealand and Australia. I first began working with them because they wanted to overhaul their branding, which at the time was very simple and following a generic blue and grey colour scheme. Their existing logo had a star included to dot the i with, and they requested that I experiment with ways to keep the star element somehow. After showing them a range of concepts, we all agreed on this direction.</p>
+    <p>Liberate I.T. are a top level NetSuite Solution Provider in New Zealand and Australia. I first began working with them because they wanted to overhaul their branding, which at the time was very simple and following a generic blue and grey colour scheme. Their existing logo had a star included to dot the i with, and they requested that I experiment with ways to keep a star element. After showing them a range of concepts, we all agreed on this direction.</p>
     
     <p>The brief stated that not only did the brand need to elevate the corporate identity of the company, but that it needed to be unique and different enough to truly stand out against their competitors. Because of this and a number of audience-specific considerations, I went with a mix of inspirational imagery of children as superheros, alongside bright colouring with high contrast.</p>
     
@@ -50,7 +238,7 @@ const portfolioData = {
       "Design a logo and set of accompanying graphics for a Twitch TV streaming channel.",
     description: `<p>PupsiclePop is a Twitch TV channel devoted to music, featuring live performance in both singing and on the piano.</p>
 
-    <p>Because of the nature of Twitch, it was an important aspect of the brief for the brand to be bright, memorable, and recognisable of the performer without featuring actual photography of them. This was both for privacy and also for wider printing purposes on related merchandise.</p>`,
+    <p>Because of the nature of Twitch, it was an important aspect of the brief for the brand to be bright, memorable, and recognisable as the performer without featuring actual photography of them. This was both for privacy and also for wider printing purposes on related merchandise.</p>`,
     technicalSpec: "<p>Created using the Adobe Creative Suite.</p>",
     featureImage: `/pupsiclePop/pupsicleLogo.jpeg`,
     imageGallery: [
@@ -101,12 +289,12 @@ const portfolioData = {
     name: "Anani Online",
     subtitle: "Brand & Website Design",
     projectBrief:
-      "Create branding suitable for usage on a website still to be designed and built.",
+      "Create branding suitable for usage on a website still to be built.",
     description: `<p>This project was to be completed in a way that would allow for the symbol to stand alone, and for both versions to be easily identifiable, while fitting within a standard customer expectation.</p>
 
-    <p>It was made clear by the client that they wanted the branding to be minimal, clean and sophisticated. They also wanted it to be direct and to the point. Their aspiration was to be in the same league as the likes of Amazon, Ali Express, and other large retailers.</p> 
+    <p>It was made clear by the client that they wanted the branding to be minimal, clean and sophisticated. They also wanted it to be direct and to the point. Their aspiration was to be in the same league as the likes of Amazon, AliExpress, and other large retailers.</p> 
     
-    <p>While the designs were accepted, Anani Online didn’t end up coming to fruition as the client faced unexpected external factors to contend with.</p>`,
+    <p>While the designs were accepted, Anani Online didn't end up coming to fruition as the client faced unexpected external factors.</p>`,
     technicalSpec: "<p>Created using the Adobe Creative Suite.</p>",
     featureImage: `/anani-online/anani-online-logo.jpeg`,
     imageGallery: [
@@ -125,9 +313,9 @@ const portfolioData = {
     subtitle: "Branding",
     projectBrief:
       "Create a logo for use on a range of media, that includes a feather.",
-    description: `<p>The client wanted a feather, and had told me that they appreciate fascinators.</p>
+    description: `<p>The client wanted a feather, and had told me that they would like to include imagery related to fascinator hats.</p>
 
-    <p>Because of these two factors, I wanted to incorporate elements that reflected the client’s personal style, as well as be appropriate for their audience. This meant including elegance, shophistication, and a level of wholesomeness, while also including femininity and individuality.</p> 
+    <p>Because of these two factors, I wanted to incorporate elements that reflected the client's personal style, as well as be appropriate for their audience. This meant including elegance, sophistication, and a level of wholesomeness, while also including femininity and individuality.</p> 
     
     <p>The branding needed to be unique enough to stand up against other artists marketing their work, and stand out enough to carry across to a public exhibition.</p> 
     
